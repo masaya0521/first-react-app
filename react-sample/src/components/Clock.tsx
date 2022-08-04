e57e8408f32dd95999e1e9cb07cb9ec699dbc9e3
masaya0521 <kaji0521km@gmail.com>
2022-08-05 00:13:22 +0900

    useEffectのチュートリアル

---
+++ react-sample/src/components/Clock.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+
+//　タイマーが呼び出される周期を１秒とする
+const UPDATE_CYCLE = 1000;
+
+// localstrageで使用するキー
+const KEY_LOCALE = "KEY_LOCALE";
+
+enum Locale {
+  US = "es-US",
+  JP = "ja-JP",
+}
+
+const getLocaleFromString = (text: string) => {
+  switch (text) {
+    case Locale.US:
+      return Locale.US;
+    case Locale.JP:
+      return Locale.JP;
+    default:
+      return Locale.US;
+  }
+};
+
+export const Clock = () => {
+  const [timestamp, setTimestamp] = useState(new Date());
+  const [locale, setLocale] = useState(Locale.US);
+
+  //タイマーをセットするための副作用
+  useEffect(() => {
+    //　タイマーのセット
+    const timer = setInterval(() => {
+      setTimestamp(new Date());
+    }, UPDATE_CYCLE);
+
+    //クリーンアップ関数を渡し、アンマウント時にタイマーの解除する
+    return () => {
+      clearInterval(timer);
+    };
+    // 初期描写のみ実行する
+  }, []);
+
+  // localstrageから値を読み込むための副作用
+  useEffect(() => {
+    const savedLocale = localStorage.getItem(KEY_LOCALE);
+    if (savedLocale !== null) {
+      setLocale(getLocaleFromString(savedLocale));
+    }
+  }, []);
+
+  // localeが変化した時に、localstrageに値を保存するための副作用
+  useEffect(() => {
+    localStorage.setItem(KEY_LOCALE, locale);
+    // 依存配列にlocaleを渡し、localeが変化するたびに実行できるようにする
+  }, [locale]);
+
+  return (
+    <div>
+      <p>
+        <span id="current-time-label">現在時刻</span>
+        <span>: {timestamp.toLocaleString(locale)}</span>
+        <select
+          value={locale}
+          onChange={(e) => setLocale(getLocaleFromString(e.target.value))}
+        >
+          <option value="en-US">en-US</option>
+          <option value="ja-JP">ja-JP</option>
+        </select>
+      </p>
+    </div>
+  );
+};
--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -7,6 +7,7 @@ import Hello from "./components/Hello";
 import Name from "./components/Name";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import { Clock } from "./components/Clock";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,6 +18,7 @@ root.render(
     <Name />
     <Message />
     <Counter initialValue={0} />
+    <Clock />
   </React.StrictMode>
 );
 
