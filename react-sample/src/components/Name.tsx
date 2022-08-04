7fbdd1a68be6470df2157d644cb4098a80de5c86
masaya0521 <kaji0521km@gmail.com>
2022-08-04 22:54:17 +0900

    foro

---
+++ react-sample/src/components/Name.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Name = () => {
+  // computedみたいなもの
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
+  return (
+    <div style={{ padding: "16px", backgroundColor: "gray" }}>
+      <label htmlFor="name">名前</label>
+      <input id="name" className="input-name" type="text" onChange={onChange} />
+    </div>
+  );
+};
+
+export default Name;
--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Hello from "./components/Hello";
+import Name from "./components/Name";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Hello />
+    <Name />
   </React.StrictMode>
 );
 
